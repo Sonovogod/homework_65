49fd2905cdc33c2fb8fdc598ab66ca1a7f07d7c8
Nikita <nikita.dronoff@gmail.com>
2023-05-24 12:01:40 +0600

    #1 Убран лишний код и решена проблема с удалением - убрал вывод в консоль за исключением вывода ошибки - исправил ошибку визуального удаления поста и уменьшение щетчика

--- instagram/wwwroot/js/site.js
+++ instagram/wwwroot/js/site.js
@@ -7,7 +7,6 @@ $(document).ready(() => {
     $('.followButton').on('click', function (event){
         event.preventDefault();
         const followerName = $(this).attr('accountName');
-        console.log(followerName);
 
         $.ajax({
             type: 'POST',
@@ -15,8 +14,6 @@ $(document).ready(() => {
             data: { followerName: followerName },
             success: function (response){
                 $('#followersCount').text(response.followerCount);
-                console.log(response);
-                console.log(response.followerCount)
                 
                 if (response.isFollow){
                     $('.btn-' + followerName).text('Отписаться')
@@ -34,14 +31,12 @@ $(document).ready(() => {
     $('.likePost').on('click', function (event) {
         event.preventDefault();
         const postId = $(this).attr('id');
-        console.log(postId);
         
         $.ajax({
             type: 'POST',
             url: '/Posts/Like/',
             data: { postId: postId},
             success: function (response){
-                console.log(response);
                 $('#likesCount-' + postId).text(response + ' Нравится');
             },
             error: function (response) {
@@ -54,16 +49,14 @@ $(document).ready(() => {
         event.preventDefault();
         const postId = $(this).attr('postId');
         const postOwner = $(this).attr('accountName');
-        console.log(postId);
         
         $.ajax({
             type: 'POST',
             url: '/Posts/Delete/',
             data: { postId: postId, postOwner: postOwner },
             success: function (response){
-                console.log(response);
-                $('#profilePost-' + postId).remove();
-                $('#posts-' + accountName).text(response);
+                $('#Post-' + postId).remove();
+                $('#posts-' + postOwner).text(response);
             },
             error: function (response){
                 console.log(response);
